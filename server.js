6b97534ad029396883270a4506c5c309cf6a2d21
fwilbor <franklin.cfg@gmail.com>
2019-08-26 18:00:43 -0400

    add routes for api and html

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //dependencies
 var express = require("express");
 
+var path = require("path");
+
 //configuration for express: boiler plate
 var app = express();
 
@@ -9,7 +11,6 @@ var PORT = process.env.PORT || 8080;
 //express boilerplate middlewear
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//listener
 
 
 
@@ -19,7 +20,56 @@ app.get("/", function (req, res) {
 })
 
 
+// PseudoCode 
+// 1) Need to Store Survey Questions 
+//2) Need Routes htmlRoutes point to home.html and survey.html
+function handleRequest(req, res) {
+    var path = req.url;
+
+    // Depending on the URL, display a different HTML file.
+    switch (path) {
+
+        case "/home":
+            return fs.readFile(__dirname + "/home.html", function (err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+
+        case "/survey":
+            return fs.readFile(__dirname + "/survey.html", function (err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+
+
+
+        default:
+            return fs.readFile(__dirname + "/home.html", function (err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+    }
+
+}
+
+//3) Route apiRoutes.js points to friends.js
+//4) GET Route that handles everything
+app.use(express.static("app/public"));
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+//listener
 app.listen(PORT, function () {
     console.log("Listening on PORT: " + PORT)
 
 });
+
+
+
+
+
+
+
+
