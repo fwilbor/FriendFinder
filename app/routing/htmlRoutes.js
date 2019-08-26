f1daa8e3ded23eb8c4e76f905fd3cde8036844b9
fwilbor <franklin.cfg@gmail.com>
2019-08-26 11:05:02 -0400

    update route function

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,17 @@
 var path = require("path");
 
 
+//Routes
+
+module.exports = function (app) {
+
+    app.get("/survey", function (req, res) {
+        res.sendfile(path.join(__dirname, "/../public/survey.html"))
+    });
+
+    app.get("*", function (req, res) {
+        res.sendfile(path.join(__dirname, "/../public/home.html"))
+    });
+
+
+};
