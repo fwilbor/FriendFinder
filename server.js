81719f1b77a78193a7f7a25e665ac6684304561c
fwilbor <franklin.cfg@gmail.com>
2019-08-26 10:46:02 -0400

    update npm package

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+//dependencies
 var express = require("express");
+
+//configuration for express: boiler plate
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+//express boilerplate middlewear
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//listener
+
+
+
+app.get("/", function (req, res) {
+    res.send("Hello World, This is Working");
+
+})
+
+
+app.listen(PORT, function () {
+    console.log("Listening on PORT: " + PORT)
+
+});
