893f82bfb787ac776df0a63eae014904a079166d
fwilbor <franklin.cfg@gmail.com>
2019-08-26 18:35:46 -0400

    made array of friend objects for testing

---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+var friends = [
+    {
+        name: "Robert Smith",
+        photo: "Image.png",
+        scores: [5, 3, 2, 4, 2, 5, 1, 3, 4, 2]
+
+
+
+    },
+
+    {
+        name: "Alana Nicole Jackson",
+        photo: "Image.png",
+        scores: [5, 2, 4, 1, 3, 2, 4, 1, 5, 2]
+
+
+
+    },
+
+    {
+        name: "Ray Williams",
+        photo: "Image.png",
+        scores: [3, 5, 1, 3, 4, 4, 1, 2, 3, 2]
+
+
+
+    },
+
+    {
+        name: "Victoria Cottonball",
+        photo: "Image.png",
+        scores: [4, 1, 2, 4, 3, 5, 2, 3, 4, 3]
+
+
+
+    },
+
+    {
+        name: "Jon Jordan",
+        photo: "Image.png",
+        scores: [4, 3, 1, 5, 5, 5, 2, 3, 4, 2]
+
+
+
+    }
+
+
+
+
+
+
+]
+
+
+module.exports = friends;
