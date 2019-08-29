590307d6450f82d1497d9fa7be01fa18fbd648fb
fwilbor <franklin.cfg@gmail.com>
2019-08-29 11:19:40 -0400

    update photos to xmen chracters

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
     {
         name: "Robert Smith",
-        photo: "Image.png",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf1HsJ3UJFQNhmeL9ZIFmXmDQuxzA-KHwv7RNoxlXxYWFeDcz_7w",
         scores: [5, 3, 2, 4, 2, 5, 1, 3, 4, 2]
 
 
@@ -10,7 +10,7 @@ var friends = [
 
     {
         name: "Alana Nicole Jackson",
-        photo: "Image.png",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjzVZS5yFg9lH4lXJNLUGZWURsbnv4KTKWGfRbU7LTPBBKlw42",
         scores: [5, 2, 4, 1, 3, 2, 4, 1, 5, 2]
 
 
@@ -19,7 +19,7 @@ var friends = [
 
     {
         name: "Ray Williams",
-        photo: "Image.png",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW-_MHcbGzhb3ngjPBIXfS-_n3kDFFcAsXaijFAsmLAFZl4-Si",
         scores: [3, 5, 1, 3, 4, 4, 1, 2, 3, 2]
 
 
@@ -28,7 +28,7 @@ var friends = [
 
     {
         name: "Victoria Cottonball",
-        photo: "Image.png",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC_URClk5E71yV9nQCSdJXXX3hQuvHcB5OHtedySVHzvH9iiyicQ",
         scores: [4, 1, 2, 4, 3, 5, 2, 3, 4, 3]
 
 
@@ -37,7 +37,7 @@ var friends = [
 
     {
         name: "Jon Jordan",
-        photo: "Image.png",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf42kDRaT0ix5XA0UzbExKNVLPB9HBtskk8cD-T1fw1b4rmkn-nw",
         scores: [4, 3, 1, 5, 5, 5, 2, 3, 4, 2]
 
 
